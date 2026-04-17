7103a9ecccb3948b8a314e01192909f3f44c6c4d
prashantsaini-art <prashant.saini@trafficpirates.com>
2026-04-18 02:05:08 +0530

    ol

--- resources/views/layouts/agent.blade.php
+++ resources/views/layouts/agent.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <title>Flight CRM – Unified Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     {{-- Bootstrap 5 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     {{-- Icons --}}
