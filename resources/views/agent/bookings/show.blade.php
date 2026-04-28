c057667f5f24292b0b39927215a28d76d302f9fd
crm-callinggenie <crm@callinggenie.com>
2026-04-29 00:57:57 +0530

    Revert "."
    This reverts commit 0f03a4ed769a6f5bd9cfa5627dd74c93de464e45.

--- resources/views/agent/bookings/show.blade.php
+++ resources/views/agent/bookings/show.blade.php
@@ -179,10 +179,6 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td><strong>Payment Card Details</strong></td>
-                                        <td>{{ $booking->payment_card_details ?: 'None' }}</td>
-                                    </tr>
-                                    <tr>
                                         <td><strong>Remarks</strong></td>
                                         <td>{{ $booking->agent_remarks ?: 'None' }}</td>
                                     </tr>
