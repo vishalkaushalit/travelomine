63c3556408c40385ae067d2ffc5f06dadd175e31
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-05-09 23:25:29 +0530

    Update 2026_05_07_204645_drop_spatie_tables.php

--- database/migrations/2026_05_07_204645_drop_spatie_tables.php
+++ database/migrations/2026_05_07_204645_drop_spatie_tables.php
@@ -8,11 +8,11 @@ return new class extends Migration
     public function up()
     {
         // Drop Spatie tables in correct order (respect foreign keys)
-        Schema::dropIfExists('model_has_permissions');
-        Schema::dropIfExists('model_has_roles');
-        Schema::dropIfExists('role_has_permissions');
-        Schema::dropIfExists('permissions');
-        Schema::dropIfExists('roles');
+        // Schema::dropIfExists('model_has_permissions');
+        // Schema::dropIfExists('model_has_roles');
+        // Schema::dropIfExists('role_has_permissions');
+        // Schema::dropIfExists('permissions');
+        // Schema::dropIfExists('roles');
     }
 
     public function down()
